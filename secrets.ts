09615da107401ab1192b544bbcbbe4e30d1c856f
Philainel <toropovmat@gmail.com>
2023-02-13 19:18:22 +0300

    Remove config & secrets defs

--- secrets.ts
+++
@@ -1,9 +0,0 @@
-import { config } from "dotenv";
-import Secrets from "./src/types/Secrets";
-config()
-
-export default {
-	discord_token: process.env.DISCORD_TOKEN || "",
-	servertap_token: process.env.SERVERTAP_TOKEN || "",
-	webhook_url: process.env.WEBHOOK_URL || ""
-} as Secrets
