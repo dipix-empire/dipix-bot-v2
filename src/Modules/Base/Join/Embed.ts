f4efe01c3935f83b1f04e8b52bcca5363e11d24a
bebrochka <paveltarasenko799@gmail.com>
2023-05-24 17:39:11 +0200

    join variable update

--- src/Modules/Base/Join/Embed.ts
+++ src/Modules/Base/Join/Embed.ts
@@ -1,16 +1,16 @@
 import { footer } from "../../../Data/Embeds"
 import { Interaction, EmbedBuilder } from "discord.js"
 
-export default (Sex: string, Promo: string, TextInput: string[], EmbedTitle: string, interaction: Interaction) => new EmbedBuilder()
-	.setTitle(EmbedTitle)
-	.setURL(`https://discordapp.com/users/${interaction.user.id}/`)
-	.addFields(
-		{ name: 'Ник в Майнкрафт', value: `${TextInput[0]}`, inline: true },
-		{ name: 'Пол', value: `${Sex}`, inline: true },
-		{ name: 'Возраст', value: `${TextInput[1]}`, inline: true },
-		{ name: 'Пригласил / Промокод', value: `${Promo}`, inline: true },
-		{ name: 'Клиент', value: `${TextInput[3]}`, inline: true }
-	)
-	.setColor('#3ba55d')
-	.setFooter(footer)
-	.setTimestamp(Date.now())
+export default (Sex: string, Promo: string, textInput: string[], embedTitle: string, interaction: Interaction) => new EmbedBuilder()
+  .setTitle(embedTitle)
+  .setURL(`https://discordapp.com/users/${interaction.user.id}/`)
+  .addFields(
+    { name: 'Ник в Майнкрафт', value: `${textInput[0]}`, inline: true },
+    { name: 'Пол', value: `${Sex}`, inline: true },
+    { name: 'Возраст', value: `${textInput[1]}`, inline: true },
+    { name: 'Пригласил / Промокод', value: `${Promo}`, inline: true },
+    { name: 'Клиент', value: `${textInput[3]}`, inline: true }
+  )
+  .setColor('#3ba55d')
+  .setFooter(footer)
+  .setTimestamp(Date.now())
